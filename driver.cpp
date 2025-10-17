9fbc44e9112dd2ce21859f579e3f2d2c626fd03f
Sana (Q) <@CocoaPuff2>
2025-10-16 19:51:19 -0700

    added comments

--- driver.cpp
+++ driver.cpp
@@ -19,18 +19,30 @@
  *  thread a thread control block (TCB) that includes its jmp_env
  *  and activation record.
  *
+ * How switching works?
+ * Each thread has a TCB that holds jmp_buf env,
+ * pointer to its saved stack area, and size.
+ *
+ * To pause a thread you:
+ *  1) save its registers with setjmp,
+ *  2) compute size between BP and SP,
+ *  3) memcpy that stack area into the TCB’s heap buffer.
+ *
+ *  To resume a thread you longjmp to its saved env and memcpy
+ *  the stack back to its original location so execution continues correctly.
  *
  */
 
 
 #include "sthread.cpp"
 
+// prints function name, counts from 0-9
 void func1( void *arg ) {
-    sthread_init( );
+    sthread_init( ); // init thread so it can return to scheduler
     for ( int i = 0; i < 10; i++ ) {
         cout << "func1: " << (char *)arg << " " <<  i << endl;
-        sleep( 1 );
-        sthread_yield( );
+        sleep( 1 ); //  simulates work with 1 second delay
+        sthread_yield( ); // gives up CPU after each iteration
     }
     sthread_exit( );
 }
@@ -64,7 +76,7 @@ int main( void ) {
     sthread_create( func2, (void *)"Seattle" );
     sthread_create( func3, (void *)"Tacoma" );
 
-    // start the sceduler
+    // start the scheduler, runs in RR order
     scheduler_start( );
 
     return 0;
