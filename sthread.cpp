7aae4966f041adf801d7ab90501ce5be172c6e38
Sana (Q) <@CocoaPuff2>
2025-10-19 00:31:12 -0700

    test

--- sthread.cpp
+++ sthread.cpp
@@ -30,7 +30,7 @@ using namespace std;
     cur_tcb->sp = sp; \
     cur_tcb->size = (int)((char*)bp - (char*)sp); \
     \
-    cur_tcb->stack = malloc(cur_tcb->size); \
+    /* cur_tcb->stack = malloc(cur_tcb->size); */ \
     memcpy(cur_tcb->stack, sp, cur_tcb->size); /*dest, src, cpy*/ \
     thr_queue.push(cur_tcb);                \
 }
